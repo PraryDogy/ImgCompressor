7f59c67a021cce19744c06a80d1e6c5717e01223
Evgeny Loshkarev <evlosh@gmail.com>
2024-11-28 18:18:56 +0300

    ref

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-a = "test"
-
-import os
+src = "/Users/Evgeny/Downloads"
 
 
-if os.path.isdir(a):
-    print(1)
+"folder_name", "size"
+
+
+named_folders = [
+    {"folder_name": "ГО", "size": 100},
+    {"folder_name": "ХО", "size": 200}
+]
+
+single_folders = [
+    {"folder_name": src + "Test", "size": 100},
+    {"folder_name": src + "Test 2", "size": 200}
+]
+
+single_files = [
+    {"folder_name": src + "file.jpg", "size": 100},
+    {"folder_name": src + "file_2.jpg", "size": 200}
+]
+
+
