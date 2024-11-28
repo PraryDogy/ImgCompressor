4038be678c0e5207ed731b6151dce5510a15d434
Evgeny Loshkarev <evlosh@gmail.com>
2024-11-28 15:47:11 +0300

    ref

--- app_win_process.py
+++ app_win_process.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget
 
 
@@ -26,7 +27,7 @@ class ProcessWin(QWidget):
         v_lay.addWidget(self.place_label)
 
         self.stop_btn = QPushButton("Стоп")
-        self.stop_btn.clicked.connect(self.stop_.emit)
+        self.stop_btn.clicked.connect(self.stop_cmd)
         v_lay.addWidget(self.stop_btn)
 
         self.feedback.connect(lambda data: self.set_labels_cmd(**data))
@@ -43,3 +44,10 @@ class ProcessWin(QWidget):
             self.setGeometry(geo)
         except (RuntimeError, Exception) as e:
             pass
+
+    def stop_cmd(self, *args):
+        self.stop_.emit()
+        self.deleteLater()
+
+    def closeEvent(self, a0: QCloseEvent | None) -> None:
+        a0.ignore()
