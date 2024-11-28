f9d060221fde3f8de11728835e9898e4e9e4b958
Evgeny Loshkarev <evlosh@gmail.com>
2024-11-28 14:58:28 +0300

    ref

--- util.py
+++ util.py
@@ -1,11 +1,41 @@
 import os
 
 from PIL import Image
-from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSignal, Qt
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
 
 
 class Shared:
     flag = True
+
+
+class ProcessWin(QWidget):
+    stop_ = pyqtSignal()
+    set_labels = pyqtSignal(dict)
+
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowModality(Qt.WindowModality.ApplicationModal)
+
+        v_lay = QVBoxLayout()
+        self.setLayout(v_lay)
+        
+        self.total_label = QLabel(text="")
+        v_lay.addWidget(self.total_label)
+
+        self.place_label = QLabel(text="")
+        v_lay.addWidget(self.place_label)
+
+        self.stop_btn = QPushButton("Стоп")
+        self.stop_btn.clicked.connect(self.stop_.emit)
+        v_lay.addWidget(self.stop_btn)
+
+        self.set_labels.connect(self.set_labels_cmd)
+
+    def set_labels_cmd(self, current: int, total: int, place: str):
+        self.total_label.setText(f"Сжато: {current} из {total}")
+        self.place_label.setText(place)
 
 
 class CompressUtils:
